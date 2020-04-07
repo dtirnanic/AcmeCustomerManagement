8d33a9c8472905d84fbfcba147d5440d5ec7f72e
dtirnanic <59708236+dtirnanic@users.noreply.github.com>
2020-04-07 06:50:35 -0500

    Created Save, Validate and Retrieve methods in Customer calss

--- Customer.cs
+++ Customer.cs
@@ -9,7 +9,7 @@ namespace AcmeCustomerManagement
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string Email { get; set; }
+        public string EmailAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string FullName 
@@ -18,6 +18,31 @@ namespace AcmeCustomerManagement
             {
                 return LastName + ", " + FirstName;
             }
+        }
+        public string HomeAddress { get; set; }
+        public string WorkAddress { get; set; }
+
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+        public bool Save()
+        {
+            return true;
+        }
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+
         }
 
     }
