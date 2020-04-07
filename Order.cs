ce58a4e05649c6ad034e9759bb2c2a779be35d79
dtirnanic <59708236+dtirnanic@users.noreply.github.com>
2020-04-07 10:48:59 -0500

    Created Order class with default properties and methods

---
+++ Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCustomerManagement
+{
+    class Order
+    {
+        public DateTime OrderDate { get; set; }
+
+        public Order Retrieve(int productId)
+        {
+            return new Order();
+        }
+        public List<Order> Retrieve()
+        {
+            return new List<Order>();
+        }
+        public bool Save()
+        {
+            return true;
+        }
+        public bool Validate()
+        {
+            var isValid = true;
+
+           // if (DateTime.ReferenceEquals() isValid = false;
+            
+
+            return isValid;
+
+        }
+
+    }
+}
