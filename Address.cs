8dff8af666c9b58bddc48085e88efc981c80d41e
dtirnanic <59708236+dtirnanic@users.noreply.github.com>
2020-04-08 09:25:11 -0500

    Address class created and populated with default properties and methods.

---
+++ Address.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCustomerManagement
+{
+    class Address
+    {
+        public int AddressId { get; set; }
+        public int AddressType { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public Address()
+        {
+
+        }
+        public Address(int addressId)
+        {
+            AddressId = addressId;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (PostalCode == null) isValid = false;
+
+            return isValid;
+
+        }
+
+    }
+}
