639d8cf426ac81a4fdcbaca1ca9296a62ed5f14c
dtirnanic <59708236+dtirnanic@users.noreply.github.com>
2020-04-09 13:26:42 -0500

    CustomerRepository class created for Retrieve and Save methods

--- Customer.cs
+++ Customer.cs
@@ -32,19 +32,6 @@ namespace AcmeCustomerManagement
             CustomerId = customerId;
         }
 
-
-        public Customer Retrieve(int customerId)
-        {
-            return new Customer();
-        }
-        public List<Customer> Retrieve()
-        {
-            return new List<Customer>();
-        }
-        public bool Save()
-        {
-            return true;
-        }
         public bool Validate()
         {
             var isValid = true;
---
+++ CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCustomerManagement
+{
+    public class CustomerRepository
+    {
+        
+        public Customer Retrieve(int customerId)
+        {
+            Customer customer = new Customer(customerId);
+
+            //temporary hard coded data to return a popuilated customer
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "fbaggins@hobbiton.me";
+                customer.FirstName = "Frodo";
+                customer.LastName = "Baggins";
+
+            }
+            return customer;
+        }
+        
+        public bool Save(Customer customer)
+        {
+            return true;
+        }
+    }
+}
